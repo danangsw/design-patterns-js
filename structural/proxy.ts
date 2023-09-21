145141616c39670d84231a4beb2714473dcda0fe
danang sukma wantara <danangsukma@gmail.com>
2023-09-21 22:55:49 +0700

    add more

--- structural/proxy.ts
+++ structural/proxy.ts
@@ -1,15 +1,87 @@
-const original = {name: 'john'};
+// https://www.dofactory.com/javascript/design-patterns/proxy
+interface addressLatLong {
+    address:string;
+    LatLong:string;
+}
 
-const reactive = new Proxy(original, {
-    get (target, key){
-        console.log('Tracking: ', key);
-        return target[key];
-    },
-    set (target, key, value){
-        console.log('updating UI...');
-        return Reflect.set(target, key, value);
+class GeoCoder {
+    getLatLong(address: string): string {
+        switch (address) {
+            case "Amsterdam":
+                return "52.3700° N, 4.8900° E";
+            case "London":
+                return "51.5171° N, 0.1062° W";
+            case "Paris":
+                return "48.8742° N, 2.3470° E";
+            case "Berlin":
+                return "52.5233° N, 13.4127° E"; 
+            default:
+                return '';
+        }
     }
-});
+}
 
-reactive.name;
-reactive.name = 'doe';
+class GeoProxy {
+    geocoder:GeoCoder;
+    geocache:Array<addressLatLong>;
+
+    constructor() {
+        this.geocoder = new GeoCoder();
+        this.geocache = Array<addressLatLong>();
+    };
+
+    getLatLong(address: string) {
+        if(!(this.geocache.find((e) => e.address === address)))
+        {
+            this.geocache.push({
+                address: address,
+                LatLong: this.geocoder.getLatLong(address)
+            });
+
+            console.log(this.geocache);
+        }
+
+        // this.geocache.push({
+        //     address: address,
+        //     LatLong: this.geocoder.getLatLong(address)
+        // });
+        // console.log(this.geocache);
+    }
+
+    getCount() {
+        return this.geocache.length;
+    }
+}
+
+function run() {
+
+    const geo = new GeoProxy();
+
+    // geolocation requests
+    geo.getLatLong("Paris");
+    geo.getLatLong("London");
+    geo.getLatLong("London");
+    geo.getLatLong("London");
+    geo.getLatLong("London");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Berlin");
+    geo.getLatLong("London");
+    geo.getLatLong("London");
+    geo.getLatLong("Paris");
+    geo.getLatLong("Berlin");
+    geo.getLatLong("Berlin");
+    geo.getLatLong("Berlin");
+    geo.getLatLong("Berlin");
+    geo.getLatLong("Berlin");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Amsterdam");
+    geo.getLatLong("Amsterdam");
+
+    console.log("\nCache size: " + geo.getCount());
+}
+
+run();
