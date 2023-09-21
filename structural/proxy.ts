af243d6f467955878860acf6b5e3bc004488983d
danang sukma wantara <danangsukma@gmail.com>
2023-09-21 21:16:11 +0700

    add more

---
+++ structural/proxy.ts
@@ -0,0 +1,15 @@
+const original = {name: 'john'};
+
+const reactive = new Proxy(original, {
+    get (target, key){
+        console.log('Tracking: ', key);
+        return target[key];
+    },
+    set (target, key, value){
+        console.log('updating UI...');
+        return Reflect.set(target, key, value);
+    }
+});
+
+reactive.name;
+reactive.name = 'doe';
